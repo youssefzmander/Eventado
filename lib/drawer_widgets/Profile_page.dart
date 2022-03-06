27e73c13b40822e98705a906800b702ce3c40730
Mejdi97 <mejdi.kchouk@esprit.tn>
2022-03-06 01:50:04 +0100

    ..

--- lib/drawer_widgets/Profile_page.dart
+++ lib/drawer_widgets/Profile_page.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:pim/main.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+final String _baseUrl = "10.0.2.2:3001";
+
+Future<User> fetchUser() async {
+  final response = await http.get(Uri.http(_baseUrl, "/user"));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class User {
+  final int userId;
+  final String f_name;
+  final String username;
+  final String email;
+
+  const User(
+      {required this.userId,
+      required this.f_name,
+      required this.username,
+      required this.email});
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      userId: json['userId'],
+      f_name: json['f_name'],
+      username: json['username'],
+      email: json['email'],
+    );
+  }
+}
 
 class Profile extends StatelessWidget {
-  const Profile({Key? key}) : super(key: key);
+  late Future<User> futureUser;
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +62,119 @@ class Profile extends StatelessWidget {
           },
         ),
       ),
-      body: ListView(),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const CircleAvatar(
+                radius: 120,
+                backgroundImage: AssetImage("assets/images/user.jpg"),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Full Name",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            const Text(
+              "Username",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Email",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: const Text(
+                    'Following',
+                  ),
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 18,
+                ),
+                ElevatedButton(
+                  child: const Text(
+                    'Followers',
+                  ),
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 18,
+                ),
+                ElevatedButton(
+                  child: Text(
+                    'CONFIRM',
+                  ),
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 18,
+                ),
+                SizedBox(
+                  width: 18,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
